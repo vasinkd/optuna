055275d0ad9ee095039a1c1f2abcf1c9b8fafc21
Takeru Ohta <phjgt308@gmail.com>
2019-05-08 10:01:42 +0900

    Add `SuccessiveHalvingSampler`.

--- optuna/samplers/__init__.py
+++ optuna/samplers/__init__.py
@@ -1,3 +1,4 @@
 from optuna.samplers.base import BaseSampler  # NOQA
 from optuna.samplers.random import RandomSampler  # NOQA
+from optuna.samplers.successive_halving import SuccessiveHalvingSampler  # NOQA
 from optuna.samplers.tpe import TPESampler  # NOQA
